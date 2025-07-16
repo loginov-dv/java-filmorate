76aeaca81da38178263dff629750b2f174ea84ab
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-07-16 20:23:28 +0300

    feat(ErrorHandler): добавить обработчик исключений для контроллеров

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
