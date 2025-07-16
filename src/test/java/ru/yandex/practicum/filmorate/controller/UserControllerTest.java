13bee9f5617b0638c3be0f95de52e40201cdcabd
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-07-16 21:27:31 +0300

    feat(UserControllerTest): добавить класс для тестирования контроллера UserController

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -0,0 +1,294 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.yandex.practicum.filmorate.adapter.LocalDateAdapter;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
+
+    private static final String USERS_URL = "/users";
+
+    @BeforeEach
+    void reset() throws Exception {
+        mockMvc.perform(delete(USERS_URL + "/clear"));
+    }
+
+    // Проверяет добавление нового пользователя
+    @Test
+    void shouldAddNewValidUser() throws Exception {
+        final User validUser = User.builder()
+                .name("vasya")
+                .login("vasya")
+                .email("vasya@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        mockMvc.perform(post(USERS_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(validUser)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get(USERS_URL))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1));
+    }
+
+    // Проверяет попытку добавления нового пользователя с некорректным логином
+    @Test
+    void shouldNotAddUserWithInvalidLogin() throws Exception {
+        final User invalidUserWithNullLogin = User.builder()
+                .name("petya")
+                .email("petya@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        final User ivalidUserWithEmptyLogin = User.builder()
+                .name("sasha")
+                .login("")
+                .email("sasha@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        final User invalidUserWithWhitespace = User.builder()
+                .name("masha")
+                .login("ma sha")
+                .email("masha@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        mockMvc.perform(post(USERS_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(invalidUserWithNullLogin)))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(post(USERS_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(ivalidUserWithEmptyLogin)))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(post(USERS_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(invalidUserWithWhitespace)))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(get(USERS_URL))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
+    }
+
+    // Проверяет попытку добавления нового пользователя с некорректной электронной почтой
+    @Test
+    void shouldNotAddUserWithInvalidEmail() throws Exception {
+        final User invalidUserWithNullEmail = User.builder()
+                .name("kolya")
+                .login("kolya")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        final User ivalidUserWithEmptyEmail = User.builder()
+                .name("nastya")
+                .login("nastya")
+                .email("")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        final User invalidUserWithMissingSymbol = User.builder()
+                .name("olya")
+                .login("olya")
+                .email("olya")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        mockMvc.perform(post(USERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(invalidUserWithNullEmail)))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(post(USERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(ivalidUserWithEmptyEmail)))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(post(USERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(invalidUserWithMissingSymbol)))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(get(USERS_URL))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
+    }
+
+    // Проверяет попытку добавления нового пользователя с некорректной датой рождения
+    @Test
+    void shouldNotAddUserWithInvalidBirthday() throws Exception {
+        final User guestFromTheFuture = User.builder()
+                .name("unknown")
+                .login("unknown")
+                .email("unknown@test.com")
+                .birthday(LocalDate.of(2099, 1, 1))
+                .build();
+
+        mockMvc.perform(post(USERS_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(guestFromTheFuture)))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(get(USERS_URL))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
+    }
+
+    // Проверяет попытку добавления пользователя с email, который уже используется
+    @Test
+    void shouldNotAddUserWithEmailAlreadyInUse() throws Exception {
+        fillWithValidData();
+
+        final User user = User.builder()
+                .name("lesha")
+                .login("lesha")
+                .email("user1@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        mockMvc.perform(post(USERS_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(user)))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(get(USERS_URL))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(3));
+    }
+
+    // Проверяет попытку отправки POST-запроса с пустым body
+    @Test
+    void shouldHandleEmptyRequest() throws Exception {
+        mockMvc.perform(post(USERS_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(""))
+                .andExpect(status().isBadRequest());
+    }
+
+    // Проверяет получение всех пользователей
+    @Test
+    void shouldGetAllUsers() throws Exception {
+        fillWithValidData();
+
+        mockMvc.perform(get(USERS_URL))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(3));
+    }
+
+    // Проверяет обновление существующего пользователя
+    @Test
+    void shouldUpdateUser() throws Exception {
+        fillWithValidData();
+
+        final User user = User.builder()
+                .id(1)
+                .name("katya")
+                .login("katya")
+                .email("katya@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        mockMvc.perform(put(USERS_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(user)))
+                .andExpect(status().isOk());
+    }
+
+    // Проверяет обновление несуществующего пользователя
+    @Test
+    void shouldNotUpdateUnknownUser() throws Exception {
+        fillWithValidData();
+
+        final User user = User.builder()
+                .id(100)
+                .name("sasha")
+                .login("sasha")
+                .email("sasha@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        mockMvc.perform(put(USERS_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(user)))
+                .andExpect(status().isBadRequest());
+    }
+
+    // Проверяет попытку изменения email на другой, который уже используется
+    @Test
+    void shouldNotUpdateEmailIfAlreadyInUse() throws Exception {
+        fillWithValidData();
+
+        final User user = User.builder()
+                .id(1)
+                .name("zhenya")
+                .login("zhenya")
+                .email("user1@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        mockMvc.perform(put(USERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(user)))
+                .andExpect(status().isOk());
+    }
+
+    void fillWithValidData() throws Exception {
+        final User user1 = User.builder()
+                .name("user1")
+                .login("user1")
+                .email("user1@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+        final User user2 = User.builder()
+                .name("user2")
+                .login("user2")
+                .email("user2@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+        final User user3 = User.builder()
+                .name("user3")
+                .login("user3")
+                .email("user3@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        List<User> users = List.of(user1, user2, user3);
+
+        for (User user : users) {
+            mockMvc.perform(post(USERS_URL)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(gson.toJson(user)))
+                    .andExpect(status().isOk());
+        }
+    }
+
+}
