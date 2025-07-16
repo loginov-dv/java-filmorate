a029b0643f8800cef613404ae5463611bd315164
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-07-16 21:46:14 +0300

    feat: добавить в классы модели аннотации для валидации

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +13,12 @@ import java.time.LocalDate;
 @Builder
 public class Film {
     private Integer id;
+    @NotNull
+    @NotBlank
     private String name;
+    @Size(max = 200)
     private String description;
     private LocalDate releaseDate;
+    @Positive
     private int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +13,14 @@ import java.time.LocalDate;
 @Builder
 public class User {
     private Integer id;
+    @NotNull
+    @NotBlank
+    @Email
     private String email;
+    @NotNull
+    @NotBlank
     private String login;
     private String name;
+    @Past
     private LocalDate birthday;
 }
