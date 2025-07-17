0c57eee93b63f30f8fc74e1f29147dc1d7638abb
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-07-17 20:44:27 +0300

    feat(User): добавить аннотацию @Pattern для поля login

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,9 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
 
@@ -21,6 +18,7 @@ public class User {
     private String email;
     // Логин
     @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "^\\S+$", message = "Логин не может содержать пробелы")
     private String login;
     // Имя
     private String name;
