c157ab0b68a5247e7c0379c5fdcfa0851a2e5081
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-02 15:54:52 +0300

    refactor(ErrorHandler): перенести класс в пакет controller

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,10 +1,13 @@
-package ru.yandex.practicum.filmorate.exception;
+package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.ErrorMessage;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 
 // RestControllerAdvice для обработки исключений и преобразования их в HTTP-ответы
 @RestControllerAdvice
