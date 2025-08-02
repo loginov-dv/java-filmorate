34581ab2a4cb00c735b348e8b2fde5d54f607110
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-02 15:40:46 +0300

    feat: добавить интерфейс и реализацию (в оперативной памяти) для хранения информации о дружбе между пользователями

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFriendshipStorage.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Friendship;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+// Класс, реализующий логику хранения дружеских связей между пользователями в оперативной памяти
+@Component
+public class InMemoryFriendshipStorage implements FriendshipStorage {
+    // Множество объектов класса Friendship
+    private final Set<Friendship> friendshipSet = new HashSet<>();
+
+    // Добавить дружескую связь между пользователями
+    public void addFriendship(int userId, int friendId) {
+        friendshipSet.add(new Friendship(userId, friendId));
+        // Двусторонняя связь
+        friendshipSet.add(new Friendship(friendId, userId));
+    }
+
+    // Удалить дружескую связь между пользователями
+    public void removeFriendship(int userId, int friendId) {
+        friendshipSet.removeIf(friendship ->
+                (friendship.getUserId().equals(userId) && friendship.getFriendId().equals(friendId))
+                        || (friendship.getUserId().equals(friendId) && friendship.getFriendId().equals(userId)));
+    }
+
+    // Получить всех друзей пользователя с указанными id
+    public Set<Integer> getFriends(int userId) {
+        return friendshipSet.stream()
+                .filter(item -> item.getUserId().equals(userId))
+                .map(Friendship::getFriendId)
+                .collect(Collectors.toSet());
+    }
+
+    // Получить всех общий друзей двух пользователей
+    public Set<Integer> getCommonFriends(int firstUserId, int secondUserId) {
+        Set<Integer> firstUserFriends = getFriends(firstUserId);
+        Set<Integer> secondUserFriends = getFriends(secondUserId);
+
+        return firstUserFriends.stream()
+                .filter(secondUserFriends::contains)
+                .collect(Collectors.toSet());
+    }
+}
