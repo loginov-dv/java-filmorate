9d48839ccea1931c6f6663012b24ef8e7434920f
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-02 19:09:45 +0300

    test(FilmControllerTest): добавить тесты для проверки новых эндпоинтов

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,7 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +13,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.test.web.servlet.MvcResult;
 import ru.yandex.practicum.filmorate.adapter.LocalDateAdapter;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,8 +38,11 @@ class FilmControllerTest {
     private final Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
     // Путь
     private static final String FILMS_URL = "/films";
+    // Jackson ObjectMapper
+    /*@Autowired
+    private ObjectMapper objectMapper;*/
 
-    // Очистка мапы контроллера через метод DELETE
+    // Очистка хранилища фильмов через метод DELETE
     @BeforeEach
     void reset() throws Exception {
         mockMvc.perform(delete(FILMS_URL + "/clear"));
@@ -219,7 +230,101 @@ class FilmControllerTest {
                 .andExpect(status().isNotFound());
     }
 
-    // Заполняет мапу контроллера тестовыми валидными данными
+    // Проверяет добавление лайка
+    @Test
+    void shouldPutLike() throws Exception {
+        fillWithValidData();
+        addUsers();
+
+        mockMvc.perform(put(FILMS_URL + "/1/like/1"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get(FILMS_URL + "/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.likes", hasSize(1)));
+    }
+
+    // Проверяет удаление лайка
+    @Test
+    void shouldRemoveLike() throws Exception {
+        fillWithValidData();
+        addUsers();
+
+        mockMvc.perform(put(FILMS_URL + "/1/like/1"))
+                .andExpect(status().isOk());
+        mockMvc.perform(delete(FILMS_URL + "/1/like/1"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get(FILMS_URL + "/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.likes", hasSize(0)));
+    }
+
+    // Проверяет попытку добавления лайка несуществующему фильму
+    @Test
+    void shouldReturnNotFoundWhenRequestedPutLikeForFilmThatDoesntExist() throws Exception {
+        fillWithValidData();
+        addUsers();
+
+        mockMvc.perform(put(FILMS_URL + "/4/like/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    // Проверяет попытку добавления лайка от несуществующего пользователя
+    @Test
+    void shouldReturnNotFoundWhenRequestedPutLikeFromUserThatDoesntExist() throws Exception {
+        fillWithValidData();
+        addUsers();
+
+        mockMvc.perform(put(FILMS_URL + "/1/like/4"))
+                .andExpect(status().isNotFound());
+    }
+
+    // Проверяет получение популярных фильмов
+    @Test
+    void shouldReturnPopularFilms() throws Exception {
+        fillWithValidData();
+        addUsers();
+
+        mockMvc.perform(put(FILMS_URL + "/1/like/1"))
+                .andExpect(status().isOk());
+        mockMvc.perform(put(FILMS_URL + "/1/like/2"))
+                .andExpect(status().isOk());
+        mockMvc.perform(put(FILMS_URL + "/1/like/3"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(put(FILMS_URL + "/2/like/1"))
+                .andExpect(status().isOk());
+        mockMvc.perform(put(FILMS_URL + "/2/like/2"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(put(FILMS_URL + "/3/like/1"))
+                .andExpect(status().isOk());
+
+        MvcResult result = mockMvc.perform(get(FILMS_URL + "/popular"))
+                .andExpect(status().isOk())
+                .andReturn();
+        /*CollectionType collectionType = objectMapper.getTypeFactory().constructCollectionType(List.class, Film.class);
+        List<Film> popularFilms = objectMapper.readValue(json, collectionType);*/
+        String json = result.getResponse().getContentAsString();
+        TypeToken<List<Film>> typeToken = new TypeToken<>(){};
+        List<Film> popularFilms = gson.fromJson(json, typeToken.getType());
+
+        assertNotNull(popularFilms);
+        assertEquals(3, popularFilms.size());
+        // значение в Optional гарантированно присутствует
+        assertEquals(3, popularFilms.stream()
+                .filter(item -> item.getId().equals(1))
+                .findFirst().orElse(null).getLikes().size());
+        assertEquals(2, popularFilms.stream()
+                .filter(item -> item.getId().equals(2))
+                .findFirst().orElse(null).getLikes().size());
+        assertEquals(1, popularFilms.stream()
+                .filter(item -> item.getId().equals(3))
+                .findFirst().orElse(null).getLikes().size());
+    }
+
+    // Заполняет хранилище фильмов тестовыми валидными данными
     void fillWithValidData() throws Exception {
         Film film1 = Film.builder()
                 .name("name 1")
@@ -245,6 +350,37 @@ class FilmControllerTest {
             mockMvc.perform(post(FILMS_URL)
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(gson.toJson(film)))
+                    .andExpect(status().isOk());
+        }
+    }
+
+    // Заполняет хранилище пользователей
+    void addUsers() throws Exception {
+        final User user1 = User.builder()
+                .name("user1")
+                .login("user1")
+                .email("user1@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+        final User user2 = User.builder()
+                .name("user2")
+                .login("user2")
+                .email("user2@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+        final User user3 = User.builder()
+                .name("user3")
+                .login("user3")
+                .email("user3@test.com")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        List<User> users = List.of(user1, user2, user3);
+
+        for (User user : users) {
+            mockMvc.perform(post("/users")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(gson.toJson(user)))
                     .andExpect(status().isOk());
         }
     }
