94c52e43c03ed98f67c60ebe8b7da0a704c940ae
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-02 15:40:10 +0300

    feat(Friendship): добавить аннотацию EqualsAndHashCode

--- src/main/java/ru/yandex/practicum/filmorate/model/Friendship.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Friendship.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 // Класс, описывающий дружескую связь между пользователями
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Friendship {
     private Integer userId;
     private Integer friendId;
