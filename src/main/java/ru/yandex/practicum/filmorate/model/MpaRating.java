85c10b21cb28129b0f1839b915973aa47fd1f195
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-22 20:34:32 +0300

    feat(MpaRating): добавить класс MpaRating и его поля

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+// Модель данных для описания рейтинга Ассоциации кинокомпаний
+@Data
+@Builder
+public class MpaRating {
+    // Идентификатор
+    private Integer id;
+    // Наименование
+    @NotBlank
+    private String name;
+}
