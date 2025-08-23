31d33f464fd04ee27c81944e659bb63cd7e5ad2e
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-23 13:08:57 +0300

    feat(UpdateUserRequest): добавить класс для изменения данных пользователя

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/UpdateUserRequest.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.dto;
+
+import lombok.Data;
+import ru.yandex.practicum.filmorate.util.StringUtils;
+
+import java.time.LocalDate;
+
+// Класс, содержащий данные для обновления пользователя
+@Data
+public class UpdateUserRequest {
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+
+    public boolean hasEmail() {
+        return !StringUtils.isNullOrEmpty(email);
+    }
+
+    public boolean hasLogin() {
+        return !StringUtils.isNullOrEmpty(login);
+    }
+
+    public boolean hasName() {
+        return !StringUtils.isNullOrEmpty(name);
+    }
+
+    public boolean hasBirthday() {
+        return birthday != null;
+    }
+}
