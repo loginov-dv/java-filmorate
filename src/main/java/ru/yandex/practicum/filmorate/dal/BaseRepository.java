6d3c9d53312f0ff149cac067271f144509ff40ae
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-23 11:21:18 +0300

    feat(BaseRepository): добавить базовый класс репозитория

---
+++ src/main/java/ru/yandex/practicum/filmorate/dal/BaseRepository.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.dal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+// Базовый класс репозитория для работы с таблицами
+@RequiredArgsConstructor
+public class BaseRepository<T> {
+    // JdbcTemplate
+    private final JdbcTemplate jdbcTemplate;
+    // Маппер
+    private final RowMapper<T> rowMapper;
+
+    protected Optional<T> findOne(String query, Object... params) {
+        try {
+            T result = jdbcTemplate.queryForObject(query, rowMapper, params);
+            return Optional.ofNullable(result);
+        } catch (EmptyResultDataAccessException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    protected List<T> findMany(String query, Object... params) {
+        return jdbcTemplate.query(query, rowMapper, params);
+    }
+}
