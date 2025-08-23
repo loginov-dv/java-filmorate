94926786bb7ae382392dae1693e1552caec14a73
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-23 13:43:45 +0300

    feat(UserMapper): добавить новые методы для маппинга

--- src/main/java/ru/yandex/practicum/filmorate/mapper/UserMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/UserMapper.java
@@ -2,12 +2,15 @@ package ru.yandex.practicum.filmorate.mapper;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import ru.yandex.practicum.filmorate.dto.NewUserRequest;
+import ru.yandex.practicum.filmorate.dto.UpdateUserRequest;
 import ru.yandex.practicum.filmorate.dto.UserDto;
 import ru.yandex.practicum.filmorate.model.User;
 
-// Маппер User to UserDto
+// Маппер для класса User
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class UserMapper {
+    // Преобразовать User в UserDto
     public static UserDto mapToUserDto(User user) {
         UserDto dto = new UserDto();
         dto.setId(user.getId());
@@ -17,5 +20,36 @@ public final class UserMapper {
         dto.setBirthday(user.getBirthday());
 
         return dto;
+    }
+
+    // Преобразовать NewUserRequest в User
+    public static User mapToUser(NewUserRequest request) {
+        return User.builder()
+                .email(request.getEmail())
+                .login(request.getLogin())
+                .name(request.getName())
+                .birthday(request.getBirthday())
+                .build();
+    }
+
+    // Обновить поля объекта класса User
+    public static User updateUserFields(User user, UpdateUserRequest request) {
+        if (request.hasEmail()) {
+            user.setEmail(request.getEmail());
+        }
+
+        if (request.hasLogin()) {
+            user.setLogin(request.getLogin());
+        }
+
+        if (request.hasName()) {
+            user.setName(request.getName());
+        }
+
+        if (request.hasBirthday()) {
+            user.setBirthday(request.getBirthday());
+        }
+
+        return user;
     }
 }
