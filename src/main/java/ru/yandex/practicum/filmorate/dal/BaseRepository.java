3a9baa96ff65520e8a5b5352c022dd2b76db628a
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-23 12:49:00 +0300

    feat(BaseRepository): добавить методы добавления и удаления в базовый класс-репозиторий

--- src/main/java/ru/yandex/practicum/filmorate/dal/BaseRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/BaseRepository.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,5 +30,39 @@ public class BaseRepository<T> {
 
     protected List<T> findMany(String query, Object... params) {
         return jdbcTemplate.query(query, rowMapper, params);
+    }
+
+    protected boolean delete(String query, int id) {
+        int rowsDeleted = jdbcTemplate.update(query, id);
+        return rowsDeleted > 0;
+    }
+
+    protected void update(String query, Object... params) {
+        int rowsUpdated = jdbcTemplate.update(query, params);
+        if (rowsUpdated == 0) {
+            // TODO: exception
+            throw new RuntimeException("Не удалось обновить данные");
+        }
+    }
+
+    protected int insert(String query, Object... params) {
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            for (int idx = 0; idx < params.length; idx++) {
+                ps.setObject(idx + 1, params[idx]);
+            }
+            return ps;
+        }, keyHolder);
+
+        Integer id = keyHolder.getKeyAs(Integer.class);
+
+        // Возвращаем новый id
+        if (id != null) {
+            return id;
+        } else {
+            // TODO: exception
+            throw new RuntimeException("Не удалось сохранить данные");
+        }
     }
 }
