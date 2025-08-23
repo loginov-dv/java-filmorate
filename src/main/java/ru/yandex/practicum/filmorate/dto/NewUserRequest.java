9330080ba8397e5a369dc7e42df30109be86c572
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-23 13:06:59 +0300

    fix(NewUserRequest): изменить тип поля birthday

--- src/main/java/ru/yandex/practicum/filmorate/dto/NewUserRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/NewUserRequest.java
@@ -2,11 +2,13 @@ package ru.yandex.practicum.filmorate.dto;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 // Класс, содержащий данные, необходимые для создания нового пользователя
 @Data
 public class NewUserRequest {
     private String email;
     private String login;
     private String name;
-    private String birthday;
+    private LocalDate birthday;
 }
