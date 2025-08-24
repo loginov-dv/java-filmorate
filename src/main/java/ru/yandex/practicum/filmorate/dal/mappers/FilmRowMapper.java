8f7007ccb19c147419faf8acf66e2cb81ad19c56
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-24 18:29:50 +0300

    feat(FilmRowMapper): добавить маппинг рейтинга

--- src/main/java/ru/yandex/practicum/filmorate/dal/mappers/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/mappers/FilmRowMapper.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.dal.mappers;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.MpaRating;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,6 +18,11 @@ public class FilmRowMapper implements RowMapper<Film> {
         film.setDescription(resultSet.getString("description"));
         film.setReleaseDate(resultSet.getDate("release_date").toLocalDate());
         film.setDuration(resultSet.getInt("duration"));
+
+        MpaRating mpaRating = new MpaRating();
+        mpaRating.setId(resultSet.getInt("rating_id"));
+
+        film.setRating(mpaRating);
 
         return film;
     }
