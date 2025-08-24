21949b0ac8193a15e2c164dafe0f14d8e53d578f
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-24 11:45:42 +0300

    feat(NewFilmRequest): добавить класс для нового добавленного фильма

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/NewFilmRequest.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.model.ReleaseDate;
+
+import java.time.LocalDate;
+
+// Класс, содержащий данные, необходимые для создания нового фильма
+@Data
+public class NewFilmRequest {
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов")
+    @NotBlank(message = "Описание должно быть заполнено")
+    private String description;
+    @ReleaseDate
+    private LocalDate releaseDate;
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
+    private int duration;
+}
