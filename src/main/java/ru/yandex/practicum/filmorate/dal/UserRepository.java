a42b498a7528f2e0f2b879a14108ecbdd99b2243
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-24 11:12:48 +0300

    feat(UserRepository): добавить метод получения пользователя по email

--- src/main/java/ru/yandex/practicum/filmorate/dal/UserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserRepository.java
@@ -1,25 +1,33 @@
 package ru.yandex.practicum.filmorate.dal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.List;
 import java.util.Optional;
 
 // Класс-репозиторий для работы с таблицей "users"
+// TODO: logs
 @Repository
 public class UserRepository extends BaseRepository<User> {
     private static final String TABLE_NAME = "users";
     private static final String FIND_ALL_QUERY = "SELECT * FROM " + TABLE_NAME;
     private static final String FIND_BY_ID_QUERY = "SELECT * FROM " + TABLE_NAME + " WHERE id = ?";
+    private static final String FIND_BY_EMAIL_QUERY = "SELECT * FROM " + TABLE_NAME + " WHERE email = ?";
     private static final String INSERT_QUERY = "INSERT INTO " + TABLE_NAME +
             "(email, login, name, birthday)" +
-            "VALUES (?, ?, ?, ?) returning id";
+            "VALUES (?, ?, ?, ?)";
     private static final String UPDATE_QUERY = "UPDATE " + TABLE_NAME + " " +
             "SET email = ?, login = ?, name = ?, birthday = ? WHERE id = ?";
+
+    // Логгер
+    private static final Logger logger = LoggerFactory.getLogger(UserRepository.class);
 
     @Autowired
     public UserRepository(JdbcTemplate jdbcTemplate, RowMapper<User> rowMapper) {
@@ -34,6 +42,10 @@ public class UserRepository extends BaseRepository<User> {
         return findOne(FIND_BY_ID_QUERY, userId);
     }
 
+    public Optional<User> getByEmail(String email) {
+        return findOne(FIND_BY_EMAIL_QUERY, email);
+    }
+
     public User create(User user) {
         int id = insert(INSERT_QUERY,
                 user.getEmail(),
@@ -42,6 +54,9 @@ public class UserRepository extends BaseRepository<User> {
                 user.getBirthday()
         );
         user.setId(id);
+
+        logger.debug("Inserted into users: id = {}, email = {}, login = {}, name = {}, birthday = {}",
+                user.getId(), user.getEmail(), user.getLogin(), user.getName(), user.getBirthday());
 
         return user;
     }
