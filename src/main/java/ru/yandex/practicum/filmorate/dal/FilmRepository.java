48d63d7e487b01dcd65a6cca010b7252464fc4b5
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-24 19:26:59 +0300

    feat(FilmRepository): добавить метод добавления нового фильма

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,8 +19,12 @@ public class FilmRepository extends BaseRepository<Film> {
     private static final String TABLE_NAME = "films";
     // Запросы
     private static final String FIND_ALL_QUERY = "SELECT * FROM " + TABLE_NAME;
-    private static final String FIND_ALL_IDS_QUERY = "SELECT film_id FROM " + TABLE_NAME;
     private static final String FIND_BY_ID_QUERY = "SELECT * FROM " + TABLE_NAME + " WHERE id = ?";
+    private static final String INSERT_FILM_QUERY = "INSERT INTO " + TABLE_NAME +
+            "(name, description, release_date, duration, rating_id) " +
+            "VALUES(?, ?, ?, ?, ?)";
+    private static final String INSERT_FILM_GENRE_QUERY = "INSERT INTO film_genres(film_id, genre_id) " +
+            "VALUES(?, ?)";
     // Логгер
     private static final Logger logger = LoggerFactory.getLogger(FilmRepository.class);
 
@@ -36,5 +41,27 @@ public class FilmRepository extends BaseRepository<Film> {
     public Optional<Film> getById(int filmId) {
         logger.debug("Запрос на получение фильма с id = {}", filmId);
         return findOne(FIND_BY_ID_QUERY, filmId);
+    }
+
+    public Film create(Film film) {
+        int id = insert(INSERT_FILM_QUERY,
+                film.getName(),
+                film.getDescription(),
+                film.getReleaseDate(),
+                film.getDuration(),
+                film.getRating().getId()
+        );
+        film.setId(id);
+
+        logger.debug("Добавлен новый фильм: id = {}, name = {}, description = {}, release_date = {}, duration = {}, " +
+                        "rating_id = {}", film.getId(), film.getName(), film.getDescription(), film.getReleaseDate(),
+                film.getDuration(), film.getRating().getId());
+
+        for (Genre genre : film.getGenres()) {
+            insert(INSERT_FILM_GENRE_QUERY, film.getId(), genre.getId());
+            logger.debug("Добавлен жанр с id = {} для фильма с id = {}", genre.getId(), film.getId());
+        }
+
+        return film;
     }
 }
