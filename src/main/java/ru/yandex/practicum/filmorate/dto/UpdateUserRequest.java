2f85a4bb1445d001e9ab5f4ac1f9ae00baa3e5b8
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-24 11:12:00 +0300

    feat(UpdateUserRequest): добавить аннотации для валидации

--- src/main/java/ru/yandex/practicum/filmorate/dto/UpdateUserRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/UpdateUserRequest.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.util.StringUtils;
 
@@ -8,9 +12,14 @@ import java.time.LocalDate;
 // Класс, содержащий данные для обновления пользователя
 @Data
 public class UpdateUserRequest {
+    @NotBlank(message = "Электронная почта не может быть пустой")
+    @Email(message = "Электронная почта не соответствует формату")
     private String email;
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "^\\S+$", message = "Логин не может содержать пробелы")
     private String login;
     private String name;
+    @Past(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
 
     public boolean hasEmail() {
