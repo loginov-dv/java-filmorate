42b2b7a339cc64814230fcf55ca5a3cbf000c855
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-24 11:43:13 +0300

    feat(FilmDto): добавить класс FilmDto

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/FilmDto.java
@@ -0,0 +1,26 @@
+package ru.yandex.practicum.filmorate.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.model.ReleaseDate;
+
+import java.time.LocalDate;
+
+// Film data transfer object
+@Data
+public class FilmDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private int id;
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов")
+    @NotBlank(message = "Описание должно быть заполнено")
+    private String description;
+    @ReleaseDate
+    private LocalDate releaseDate;
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
+    private int duration;
+}
