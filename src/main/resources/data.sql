e6cca9dbe51c4943250a267d544b801d41c1dc77
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-25 21:19:52 +0300

    feat(data.sql): добавить удаление данных из всех таблиц

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,20 @@
-INSERT INTO genres(name) VALUES("Комедия");
-INSERT INTO genres(name) VALUES("Драма");
-INSERT INTO genres(name) VALUES("Мультфильм");
-INSERT INTO genres(name) VALUES("Триллер");
-INSERT INTO genres(name) VALUES("Документальный");
-INSERT INTO genres(name) VALUES("Боевик");
+DELETE FROM friendships;
+DELETE FROM film_likes;
+DELETE FROM film_genres;
+DELETE FROM films;
+DELETE FROM users;
+DELETE FROM genres;
+DELETE FROM ratings;
 
-INSERT INTO mpa(name) VALUES("G");
-INSERT INTO mpa(name) VALUES("PG");
-INSERT INTO mpa(name) VALUES("PG-13");
-INSERT INTO mpa(name) VALUES("R");
-INSERT INTO mpa(name) VALUES("NC-17");
+INSERT INTO genres(name) VALUES('Комедия');
+INSERT INTO genres(name) VALUES('Драма');
+INSERT INTO genres(name) VALUES('Мультфильм');
+INSERT INTO genres(name) VALUES('Триллер');
+INSERT INTO genres(name) VALUES('Документальный');
+INSERT INTO genres(name) VALUES('Боевик');
+
+INSERT INTO ratings(name) VALUES('G');
+INSERT INTO ratings(name) VALUES('PG');
+INSERT INTO ratings(name) VALUES('PG-13');
+INSERT INTO ratings(name) VALUES('R');
+INSERT INTO ratings(name) VALUES('NC-17');
