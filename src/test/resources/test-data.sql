865a9e8d10d6d294dab88a8bf18cdaee05eef186
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-26 22:27:45 +0300

    test(test-data.sql): добавить файл со скриптом наполнения БД тестовыми данными пользователей

---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,14 @@
+INSERT INTO users(email, login, name, birthday)
+VALUES('user1@example.com', 'user1', 'user1', '2000-01-01');
+
+INSERT INTO users(email, login, name, birthday)
+VALUES('user2@example.com', 'user2', 'user2', '2000-01-01');
+
+INSERT INTO users(email, login, name, birthday)
+VALUES('user3@example.com', 'user3', 'user3', '2000-01-01');
+
+INSERT INTO users(email, login, name, birthday)
+VALUES('user4@example.com', 'user4', 'user4', '2000-01-01');
+
+INSERT INTO users(email, login, name, birthday)
+VALUES('user5@example.com', 'user5', 'user5', '2000-01-01');
