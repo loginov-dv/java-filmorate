f58043d803d4d8387dce2928a72481fb2933840b
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-27 20:12:16 +0300

    refactor(Genre): убрать аннотацию @Builder

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
 import lombok.Data;
 
 // Модель данных для описания жанра фильма
 @Data
-@Builder
 public class Genre {
     // Идентификатор
     private Integer id;
