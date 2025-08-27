e894f186e436331df3fb60773331752b549387d7
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-27 20:13:07 +0300

    test(test-data.sql): добавить запросы для наполнения таблицы films

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -1,3 +1,4 @@
+-- users
 INSERT INTO users(email, login, name, birthday)
 VALUES('user1@example.com', 'user1', 'user1', '2000-01-01');
 
@@ -12,3 +13,19 @@ VALUES('user4@example.com', 'user4', 'user4', '2000-01-01');
 
 INSERT INTO users(email, login, name, birthday)
 VALUES('user5@example.com', 'user5', 'user5', '2000-01-01');
+
+-- films
+INSERT INTO films(name, description, release_date, duration, rating_id)
+VALUES('film1', 'film1', '2020-01-01', 120, 1);
+
+INSERT INTO films(name, description, release_date, duration, rating_id)
+VALUES('film2', 'film2', '2020-01-01', 120, 2);
+
+INSERT INTO films(name, description, release_date, duration, rating_id)
+VALUES('film3', 'film3', '2020-01-01', 120, 3);
+
+INSERT INTO films(name, description, release_date, duration, rating_id)
+VALUES('film4', 'film4', '2020-01-01', 120, 4);
+
+INSERT INTO films(name, description, release_date, duration, rating_id)
+VALUES('film5', 'film5', '2020-01-01', 120, 5);
