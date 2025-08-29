8c965c888069363e0b9880d3b18dfa44025d2bfb
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-29 17:56:32 +0300

    feat(UpdateUserRequest): добавить аннотации валидации

--- src/main/java/ru/yandex/practicum/filmorate/dto/UpdateUserRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/UpdateUserRequest.java
@@ -1,29 +1,37 @@
 package ru.yandex.practicum.filmorate.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.util.StringUtils;
 
 import java.time.LocalDate;
 
 // Класс, содержащий данные для обновления пользователя
 @Data
 public class UpdateUserRequest {
+    @NotNull(message = "Не указан id")
     private Integer id;
+    @Pattern(regexp = "^\\S+$", message = "Электронная почта не может быть пустой или содержать пробелы")
+    @Email(message = "Электронная почта не соответствует формату")
     private String email;
+    @Pattern(regexp = "^\\S+$", message = "Логин не может содержать пробелы")
     private String login;
     private String name;
+    @Past(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
 
     public boolean hasEmail() {
-        return !StringUtils.isNullOrEmpty(email);
+        return email != null;
     }
 
     public boolean hasLogin() {
-        return !StringUtils.isNullOrEmpty(login);
+        return login != null;
     }
 
     public boolean hasName() {
-        return !StringUtils.isNullOrEmpty(name);
+        return name != null;
     }
 
     public boolean hasBirthday() {
