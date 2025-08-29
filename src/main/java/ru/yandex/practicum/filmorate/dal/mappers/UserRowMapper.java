3a0586b8461f055041fd0ecfc2de188f7042db33
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-08-29 16:55:53 +0300

    feat(UserRowMapper): добавить проверку на null значения из столбца birthday

--- src/main/java/ru/yandex/practicum/filmorate/dal/mappers/UserRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/mappers/UserRowMapper.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -12,12 +13,17 @@ import java.sql.SQLException;
 public class UserRowMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-        return User.builder()
-                .id(resultSet.getInt("user_id"))
-                .name(resultSet.getString("name"))
-                .login(resultSet.getString("login"))
-                .email(resultSet.getString("email"))
-                .birthday(resultSet.getDate("birthday").toLocalDate())
-                .build();
+        User user = new User();
+        user.setId(resultSet.getInt("user_id"));
+        user.setName(resultSet.getString("name"));
+        user.setLogin(resultSet.getString("login"));
+        user.setEmail(resultSet.getString("email"));
+
+        Date birthday = resultSet.getDate("birthday");
+        if (birthday != null) {
+            user.setBirthday(birthday.toLocalDate());
+        }
+
+        return user;
     }
 }
