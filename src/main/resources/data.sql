0a4bc8b0906223a0523158f18c44409022f7b161
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-09-15 23:03:17 +0300

    fix: скорректировать порядок инструкций в sql-скриптах для обеспечения целостности данных

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,22 +1,22 @@
 DELETE FROM friendships;
 DELETE FROM film_likes;
 DELETE FROM film_genres;
+DELETE FROM film_directors;
 DELETE FROM films;
 DELETE FROM users;
 DELETE FROM genres;
 DELETE FROM ratings;
 DELETE FROM directors;
-DELETE FROM film_directors;
 
 ALTER TABLE friendships ALTER COLUMN id RESTART WITH 1;
 ALTER TABLE film_likes ALTER COLUMN id RESTART WITH 1;
 ALTER TABLE film_genres ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE film_directors ALTER COLUMN id RESTART WITH 1;
 ALTER TABLE films ALTER COLUMN film_id RESTART WITH 1;
 ALTER TABLE users ALTER COLUMN user_id RESTART WITH 1;
 ALTER TABLE genres ALTER COLUMN genre_id RESTART WITH 1;
 ALTER TABLE ratings ALTER COLUMN rating_id RESTART WITH 1;
 ALTER TABLE directors ALTER COLUMN director_id RESTART WITH 1;
-ALTER TABLE film_directors ALTER COLUMN id RESTART WITH 1;
 
 INSERT INTO genres(name) VALUES('Комедия');
 INSERT INTO genres(name) VALUES('Драма');
