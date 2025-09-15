8c1dfb946da73be6b15893543df5898f9375032e
GreateLordAvocado <sinner.halo@yandex.ru>
2025-09-15 17:57:52 +0700

    feat(NewReviewRequest): добавить DTO создания отзыва с валидацией

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/ReviewDto.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.dto;
+
+import lombok.Data;
+
+@Data
+public class ReviewDto {
+    private Integer reviewId;
+    private String content;
+    private Boolean isPositive;
+    private Integer userId; // Пользователь
+    private Integer filmId; // Фильм
+    private Integer useful; // Рейтинг
+}
