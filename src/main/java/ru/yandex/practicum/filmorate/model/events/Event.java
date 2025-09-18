e6ce0ef21fe7d161cf4f9862366ef0df7c984f01
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-09-18 20:10:17 +0300

    feat(Event): добавить конструктор

--- src/main/java/ru/yandex/practicum/filmorate/model/events/Event.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/events/Event.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.model.events;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 // Модель данных для события
 @Data
+@NoArgsConstructor
 public class Event {
     private Integer userId;
     private Integer eventId;
@@ -13,4 +15,12 @@ public class Event {
     private Integer entityId;
     private EventType eventType;
     private Operation operation;
+
+    public Event(int userId, int entityId, EventType type, Operation operation) {
+        this.userId = userId;
+        this.entityId = entityId;
+        this.eventType = type;
+        this.operation = operation;
+        timestamp = Instant.now();
+    }
 }
