6d37cc6a1516da05adb4bb0a705e664838480c8d
Dmitry Prokhorov <prokhorovds26@mail.ru>
2025-09-23 20:23:04 +0500

    feat(FilmRepository): подстроил алиасы в запросе под FilmResultSetExtractor

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
@@ -193,10 +193,17 @@ public class FilmRepository extends BaseRepository<Film> {
                 FROM film_likes
                 WHERE user_id = ?
             )
-            SELECT f.film_id, f.name, f.description, f.release_date, f.duration,
-                   r.rating_id, r.name AS rating_name,
-                   g.genre_id, g.name AS genre_name,
-                   d.director_id, d.name AS director_name
+            SELECT f.film_id,
+                   f.name AS film_name,
+                   f.description AS film_description,
+                   f.release_date AS film_release_date,
+                   f.duration AS film_duration,
+                   r.rating_id,
+                   r.name AS rating_name,
+                   g.genre_id,
+                   g.name AS genre_name,
+                   d.director_id,
+                   d.name AS director_name
             FROM film_likes fl
             JOIN films f ON fl.film_id = f.film_id
             LEFT JOIN ratings r ON f.rating_id = r.rating_id
